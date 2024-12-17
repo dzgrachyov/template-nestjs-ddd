7d1b2085462a77a6fd5d7b9e62ddf91bd00c4bd5
Dzmitry Hrachou <dzmitry.grachyov@outlook.com>
2024-12-17 23:56:28 +0300

    migration added

--- apps/template-api/src/template-api.module.ts
+++ apps/template-api/src/template-api.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TemplateApiController } from './template-api.controller';
 import { TemplateApiService } from './template-api.service';
+import { IdentityModule } from './identity/identity.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [IdentityModule, UserModule],
   controllers: [TemplateApiController],
   providers: [TemplateApiService],
 })
---
+++ libs/template-domain/src/dtos/base-entity.dto.ts
@@ -0,0 +1,5 @@
+class BaseEntityDto {
+  id: number;
+  created_at: Date;
+  updated_at: Date;
+}
