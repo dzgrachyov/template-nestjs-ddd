fdddedecc4d0bb0233dbd0494da9384ae282a8a2
Dzmitry Hrachou <dzmitry.grachyov@outlook.com>
2024-12-18 09:37:44 +0300

    nestjs config added

--- apps/template-api/src/main.ts
+++ apps/template-api/src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { TemplateApiModule } from './template-api.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(TemplateApiModule);
-  const config = new DocumentBuilder()
+  const swagger = new DocumentBuilder()
     .setTitle('Template example')
     .setDescription('The template API description')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () => SwaggerModule.createDocument(app, swagger);
   SwaggerModule.setup('api', app, documentFactory);
 
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  await app.listen(config.get('PORT') ?? 3000, () => { console.log(`Server listening port ${config.get('PORT')}`) });
 }
 bootstrap();
--- apps/template-api/src/template-api.module.ts
+++ apps/template-api/src/template-api.module.ts
@@ -3,10 +3,19 @@ import { TemplateApiController } from './template-api.controller';
 import { TemplateApiService } from './template-api.service';
 import { IdentityModule } from './identity/identity.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import http_api_config from 'config/http-api.config';
 
 @Module({
-  imports: [IdentityModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [http_api_config],
+      envFilePath: './apps/template-api/.env',
+    }),
+    IdentityModule,
+    UserModule,
+  ],
   controllers: [TemplateApiController],
   providers: [TemplateApiService],
 })
-export class TemplateApiModule {}
+export class TemplateApiModule { }
