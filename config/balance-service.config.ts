bd90fb83b8521f845f9a452c99449125ae9b4308
Dzmitry Hrachou <dzmitry.grachyov@outlook.com>
2024-12-20 13:57:47 +0300

    refactoring

--- config/balance-service.config.ts
+++ config/balance-service.config.ts
@@ -1,3 +1,3 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3001,
+  port: parseInt(process.env.PORT_BALANCE_SERVICE, 10) || 3001,
 });
--- config/http-api.config.ts
+++ config/http-api.config.ts
@@ -1,3 +1,3 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT_HTTP_API, 10) || 3000,
 });
